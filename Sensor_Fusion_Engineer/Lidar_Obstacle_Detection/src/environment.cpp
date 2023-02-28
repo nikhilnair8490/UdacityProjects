d91986527acc49258871d3614975cf24e4749fbd
Nikhil Nair <nikhilnair8490@gmail.com>
2023-02-28 22:22:17 +0530

    Add Lidar object

--- Sensor_Fusion_Engineer/Lidar_Obstacle_Detection/src/environment.cpp
+++ Sensor_Fusion_Engineer/Lidar_Obstacle_Detection/src/environment.cpp
@@ -45,7 +45,8 @@ void simpleHighway(pcl::visualization::PCLVisualizer::Ptr& viewer)
     bool renderScene = true;
     std::vector<Car> cars = initHighway(renderScene, viewer);
     
-    // TODO:: Create lidar sensor 
+    // Create lidar sensor
+    Lidar* lidar = new Lidar(cars, 0); 
 
     // TODO:: Create point processor
   
