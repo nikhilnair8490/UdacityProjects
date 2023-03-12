ea36b74617b9186248f3d81f171f362befd36d49
Nikhil Nair <nikhilnair8490@gmail.com>
2023-03-12 17:41:41 +0530

    Initial commit for traffic light classifier

---
+++ Intro_To_Self_Driving_Car/ComputerVision/TrafficLightClassifier/HSVColorPicker.py
@@ -0,0 +1,73 @@
+import cv2
+import sys
+import numpy as np
+
+def nothing(x):
+    pass
+
+# Load in image
+image = cv2.imread(r"D:\OneDrive\Nikhil_Personal\Courses\Udacity\IntroToSelfDrivingCar\Part7_ComputerVisionandMachineLearning\Project\traffic_light_images\misclassifiedimages\misclassIM2.png")
+
+#image = cv2.resize(image, (1000,1000))
+
+# Create a window
+cv2.namedWindow('image',cv2.WINDOW_NORMAL)
+
+# create trackbars for color change
+cv2.createTrackbar('HMin','image',0,179,nothing) # Hue is from 0-179 for Opencv
+cv2.createTrackbar('SMin','image',0,255,nothing)
+cv2.createTrackbar('VMin','image',0,255,nothing)
+cv2.createTrackbar('HMax','image',0,179,nothing)
+cv2.createTrackbar('SMax','image',0,255,nothing)
+cv2.createTrackbar('VMax','image',0,255,nothing)
+
+# Set default value for MAX HSV trackbars.
+cv2.setTrackbarPos('HMax', 'image', 179)
+cv2.setTrackbarPos('SMax', 'image', 255)
+cv2.setTrackbarPos('VMax', 'image', 255)
+
+# Initialize to check if HSV min/max value changes
+hMin = sMin = vMin = hMax = sMax = vMax = 0
+phMin = psMin = pvMin = phMax = psMax = pvMax = 0
+
+output = image
+wait_time = 33
+
+while(1):
+
+    # get current positions of all trackbars
+    hMin = cv2.getTrackbarPos('HMin','image')
+    sMin = cv2.getTrackbarPos('SMin','image')
+    vMin = cv2.getTrackbarPos('VMin','image')
+
+    hMax = cv2.getTrackbarPos('HMax','image')
+    sMax = cv2.getTrackbarPos('SMax','image')
+    vMax = cv2.getTrackbarPos('VMax','image')
+
+    # Set minimum and max HSV values to display
+    lower = np.array([hMin, sMin, vMin])
+    upper = np.array([hMax, sMax, vMax])
+
+    # Create HSV Image and threshold into a range.
+    hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    mask = cv2.inRange(hsv, lower, upper)
+    output = cv2.bitwise_and(image,image, mask= mask)
+
+    # Print if there is a change in HSV value
+    if( (phMin != hMin) | (psMin != sMin) | (pvMin != vMin) | (phMax != hMax) | (psMax != sMax) | (pvMax != vMax) ):
+        print("(hMin = %d , sMin = %d, vMin = %d), (hMax = %d , sMax = %d, vMax = %d)" % (hMin , sMin , vMin, hMax, sMax , vMax))
+        phMin = hMin
+        psMin = sMin
+        pvMin = vMin
+        phMax = hMax
+        psMax = sMax
+        pvMax = vMax
+
+    # Display output image
+    cv2.imshow('image',output)
+
+    # Wait longer to prevent freeze for videos.
+    if cv2.waitKey(wait_time) & 0xFF == ord('q'):
+        break
+
+cv2.destroyAllWindows()
