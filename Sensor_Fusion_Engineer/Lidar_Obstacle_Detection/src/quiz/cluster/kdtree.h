95ebbe8e9c60fd03e60368125e95321a7581d901
Nikhil Nair <nikhilnair8490@gmail.com>
2023-03-19 16:13:14 +0530

    Implement insert function

--- Sensor_Fusion_Engineer/Lidar_Obstacle_Detection/src/quiz/cluster/kdtree.h
+++ Sensor_Fusion_Engineer/Lidar_Obstacle_Detection/src/quiz/cluster/kdtree.h
@@ -1,4 +1,5 @@
 /* \author Aaron Brown */
+// Nikhil Nair: Implement insert function
 // Quiz on implementing kd tree
 
 #include "../../render/render.h"
@@ -38,9 +39,76 @@ struct KdTree
 
 	void insert(std::vector<float> point, int id)
 	{
-		// TODO: Fill in this function to insert a new point into the tree
-		// the function should create a new node and place correctly with in the root 
+		// Function to insert a new point into the tree
 
+		//Insert the point as root if the root is NULL
+		if (root == NULL)
+        {
+            root = new Node(point, id);
+            return;
+        }
+        else
+        {
+            // Create a new node from the point
+			Node* newNode = new Node(point, id);
+			//Traverse through the tree and find an empty slot to place the new point
+			Node* temp = root; //Start from root node
+			uint depth = 0; // depth of tree
+            while (temp!= NULL)
+            {
+                if (depth%2 ==0 ) // tree depth for x split
+				{
+					if (point[0] < temp->point[0])
+					{
+						// Assign the next empty node on left branch
+						if (temp->left == NULL)
+						{
+							temp->left = newNode;
+                            return;
+						}
+						temp = temp->left;
+					}
+					else
+					{
+						// Assign the next empty node on right branch
+						if (temp->right == NULL)
+						{
+							temp->right = newNode;
+                            return;
+						}						
+						temp = temp->right;
+					}
+				}
+				else // tree depth for y split
+				{
+					if (point[1] < temp->point[1])
+					{
+						// Assign the next empty node on left branch
+						if (temp->left == NULL)
+						{
+							temp->left = newNode;
+                            return;
+						}						
+						temp = temp->left;
+					}
+					else
+					{
+						// Assign the next empty node on right branch
+						if (temp->right == NULL)
+						{
+							temp->right = newNode;
+                            return;
+						}						
+						temp = temp->right;
+					}					
+				}
+				//If there are no empty slots in current level go to next depth level of tree
+				depth += 1;
+
+            }
+        }
+
+		return;
 	}
 
 	// return a list of point ids in the tree that are within distance of target
