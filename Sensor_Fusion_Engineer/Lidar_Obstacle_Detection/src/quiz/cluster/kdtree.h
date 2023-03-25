ba7ff6ab733ddc46bf535b61c3723d761debefb3
Nikhil Nair <nikhilnair8490@gmail.com>
2023-03-25 18:06:31 +0530

    Adapt kd tree for both 2d and 3d points

--- Sensor_Fusion_Engineer/Lidar_Obstacle_Detection/src/quiz/cluster/kdtree.h
+++ Sensor_Fusion_Engineer/Lidar_Obstacle_Detection/src/quiz/cluster/kdtree.h
@@ -1,5 +1,5 @@
 /* \author Aaron Brown */
-// Nikhil Nair: Implement insert and search function
+// Nikhil Nair: Implement insert and search function which works for 2-d and 3-dimensional points
 // Quiz on implementing kd tree
 
 #include "../../render/render.h"
@@ -42,6 +42,15 @@ struct KdTree
 	{
 		// Function to insert a new point into the tree
 
+		// Get the dimensions of point
+		uint dim = point.size();
+
+		// Limit dim to max 3 dimensions (x, y, z)
+		if (dim > 3)
+		{
+			dim = 3;
+		}
+
 		// Insert the point as root if the root is NULL
 		if (root == NULL)
 		{
@@ -57,53 +66,28 @@ struct KdTree
 			uint depth = 0;	   // depth of tree
 			while (temp != NULL)
 			{
-				if (depth % 2 == 0) // tree depth for x split
+				// Alternate between each dim of point and insert the point in the empty slot
+				if (point[depth % dim] < temp->point[depth % dim])
 				{
-					if (point[0] < temp->point[0])
+					// Assign the next empty node on left branch
+					if (temp->left == NULL)
 					{
-						// Assign the next empty node on left branch
-						if (temp->left == NULL)
-						{
-							temp->left = newNode;
-							return;
-						}
-						temp = temp->left;
+						temp->left = newNode;
+						return;
 					}
-					else
-					{
-						// Assign the next empty node on right branch
-						if (temp->right == NULL)
-						{
-							temp->right = newNode;
-							return;
-						}
-						temp = temp->right;
-					}
+					temp = temp->left;
 				}
-				else // tree depth for y split
+				else
 				{
-					if (point[1] < temp->point[1])
+					// Assign the next empty node on right branch
+					if (temp->right == NULL)
 					{
-						// Assign the next empty node on left branch
-						if (temp->left == NULL)
-						{
-							temp->left = newNode;
-							return;
-						}
-						temp = temp->left;
+						temp->right = newNode;
+						return;
 					}
-					else
-					{
-						// Assign the next empty node on right branch
-						if (temp->right == NULL)
-						{
-							temp->right = newNode;
-							return;
-						}
-						temp = temp->right;
-					}
+					temp = temp->right;
 				}
-				// If there are no empty slots in current level go to next depth level of tree
+				// If there are no empty slots in current level go to next depth (level) of tree
 				depth += 1;
 			}
 		}
@@ -111,23 +95,63 @@ struct KdTree
 		return;
 	}
 
-	// Recursive helper function to search for all nearest neighbors of target
+	/**
+	 * @brief Recursive helper function to search for all nearest neighbors of target
+	 *
+	 * @param node Current node in tree
+	 * @param target Target point to search for
+	 * @param distanceTol nearest neighbor distance tolerance
+	 * @param ids list of point ids in the tree that are within distance tol of target
+	 * @param depth Depth of k-d tree
+	 */
 	void searchRecursive(Node *node, std::vector<float> target, float distanceTol, std::vector<int> &ids, uint depth)
 	{
 
+		// Get the dimensions of target point
+		uint dim = target.size();
+
+		// Limit dim to max 3 dimensions (x, y, z)
+		if (dim > 3)
+		{
+			dim = 3;
+		}
+
 		// Define target box boundaries
-		float x1 = target[0] - distanceTol;
-		float x2 = target[0] + distanceTol;
-		float y1 = target[1] - distanceTol;
-		float y2 = target[1] + distanceTol;
+		// Note: Only spatial dimensions are used for the search criteria. This will work for both 2-d and 3-d cases
+
+		// Define an empty array of size 2*dim (size of target box)
+		std::vector<float> tarBox(2 * dim);
+
+		// Define the boundaries of the target box
+		for (uint i = 0; i < dim; ++i)
+		{
+			tarBox[i] = target[i] - distanceTol;
+			tarBox[i + dim] = target[i] + distanceTol;
+		}
 
 		if (node != NULL)
 		{
-			// Check if the node point is within target box
-			if (node->point[0] >= x1 && node->point[0] <= x2 && node->point[1] >= y1 && node->point[1] <= y2)
+			bool isWithin = true;
+			// Check if the node point is within target box tarBox.
+			for (uint j = 0; j < dim; ++j)
+			{
+				if (node->point[j] < tarBox[j] || node->point[j] > tarBox[j + dim])
+				{
+					isWithin = false;
+					break;
+				}
+			}
+
+			if (isWithin)
 			{
 				// Calculate euclidean distance of target from the node point
-				float distance = sqrt(pow(node->point[0] - target[0], 2) + pow(node->point[1] - target[1], 2));
+				float distance = 0.0;
+				for (uint k = 0; k < dim; ++k)
+				{
+					distance += pow(node->point[k] - target[k], 2);
+				}
+				distance = sqrt(distance);
+
 				// If the node point is within the distanceTol add it in ids
 				if (distance <= distanceTol)
 				{
@@ -136,18 +160,24 @@ struct KdTree
 			}
 
 			// Check if the target box crosses left node boundary
-			if ((target[depth % 2] - distanceTol) < node->point[depth % 2])
+			if ((target[depth % dim] - distanceTol) < node->point[depth % dim])
 				// Explore the left subtree
 				searchRecursive(node->left, target, distanceTol, ids, depth + 1);
 
 			// Check if the target box crosses right node boundary
-			if ((target[depth % 2] + distanceTol) > node->point[depth % 2])
+			if ((target[depth % dim] + distanceTol) > node->point[depth % dim])
 				// Explore the right subtree
 				searchRecursive(node->right, target, distanceTol, ids, depth + 1);
 		}
 	}
 
-	// return a list of point ids in the tree that are within distance of target
+	/**
+	 * @brief Search for all nearest neighbors of target
+	 *
+	 * @param target Target point to search for
+	 * @param distanceTol nearest neighbor distance tolerance
+	 * @return std::vector<int> list of point ids in the tree that are within distance tol of target
+	 */
 	std::vector<int> search(std::vector<float> target, float distanceTol)
 	{
 		std::vector<int> ids;
